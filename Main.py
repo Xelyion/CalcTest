ed1f15fe4912ef94371d5722e88aeeeb6c5be984
Soo Kng <sookng@gmail.com>
2021-09-17 16:30:35 +0800

    Added new functionality for multiplication

--- Main.py
+++ Main.py
@@ -9,3 +9,7 @@ print("a + b = ", sum)
 # Section 2: Function to subtract two numbers
 output2 = a - b
 print("a - b =", output2)
+
+# Section 3: Function to multipy two numbers
+output3 = a * b
+print("a * b =", output3)
